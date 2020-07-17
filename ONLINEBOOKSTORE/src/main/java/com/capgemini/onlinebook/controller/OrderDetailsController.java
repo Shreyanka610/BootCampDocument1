acb3f54b7bd8ebe5101f06fab0c58bf7d811d43f
shrey <shrey@DESKTOP-FJAASOS>
2020-07-17 22:05:40 +0530

    backend code

--- ONLINEBOOKSTORE/src/main/java/com/capgemini/onlinebook/controller/OrderDetailsController.java
+++ ONLINEBOOKSTORE/src/main/java/com/capgemini/onlinebook/controller/OrderDetailsController.java
@@ -1,10 +1,8 @@
 package com.capgemini.onlinebook.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.CrossOrigin; 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,21 +18,13 @@ import com.capgemini.onlinebook.service.OrderDetailsService;
 	@Autowired
 	private OrderDetailsService orderDetailsService;
 	
-	@GetMapping("/Order/{OrderId}")
-	public ResponseEntity<OrderDetails> get(@PathVariable Integer OrderId){
-		return new ResponseEntity<OrderDetails>(HttpStatus.OK);
-		
+	@GetMapping("/orders/{OrderId}")
+	public ResponseEntity<OrderDetails> getOrderById(@PathVariable Integer OrderId){
+		return ResponseEntity.ok().body(orderDetailsService.getOrderById(OrderId));
 	}
 	
-	/*@GetMapping("/products/{id}")
-	public ResponseEntity<Product> get(@PathVariable Integer id) {
-	    try {
-	        Product product = service.get(id);
-	        return new ResponseEntity<Product>(product, HttpStatus.OK);
-	    } catch (NoSuchElementException e) {
-	        return new ResponseEntity<Product>(HttpStatus.NOT_FOUND);
-	    }      */
-	}
+	
 	
 	
 
+	}
--- ONLINEBOOKSTORE/src/main/java/com/capgemini/onlinebook/service/OrderDetailsServiceImpl.java
+++ ONLINEBOOKSTORE/src/main/java/com/capgemini/onlinebook/service/OrderDetailsServiceImpl.java
@@ -1,14 +1,12 @@
 package com.capgemini.onlinebook.service;
-
-import java.util.Optional;
-
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import java.util.Optional;
 import com.capgemini.onlinebook.dao.OrderDetailsDao;
 import com.capgemini.onlinebook.entity.OrderDetails;
+import com.capgemini.onlinebook.exception.OrderNotFoundException;
 
 @Service
 @Transactional
@@ -17,11 +15,22 @@ public class OrderDetailsServiceImpl implements OrderDetailsService
 	@Autowired
 	private OrderDetailsDao orderDetailsDao;
 
+	
 	@Override
-	public Optional<OrderDetails> findById(Integer OrderId) {
-		
-		return orderDetailsDao.findById(OrderId) ;
+	public OrderDetails getOrderById(Integer OrderId) {
+		// TODO Auto-generated method stub
+		Optional<OrderDetails> order=this.orderDetailsDao.findById(OrderId);
+		if(order.isPresent())
+		{
+		return order.get();
+		}
+		else
+		{
+			throw new OrderNotFoundException("Record not found with id: "+OrderId);
+		}
 	}
-
-
 }
+	
+	
+	
+     
