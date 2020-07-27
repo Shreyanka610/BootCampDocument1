2eb0cb8fb3966d334b88c27a8cf1475542513ae4
Shreyanka610 <47826771+Shreyanka610@users.noreply.github.com>
2020-07-27 16:21:43 +0530

    Delete CustomerDetailsDao.java

--- ONLINEBOOKSTORE/src/main/java/com/capgemini/onlinebook/dao/CustomerDetailsDao.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.onlinebook.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capgemini.onlinebook.entity.CustomerDetails;
-
-public interface CustomerDetailsDao extends JpaRepository<CustomerDetails, Integer> {
-
-}
