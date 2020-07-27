872d68f7e5cbf8a8da914a62981ba899e4bf73f3
Shreyanka610 <47826771+Shreyanka610@users.noreply.github.com>
2020-07-27 16:21:32 +0530

    Delete BookDetailsDao.java

--- ONLINEBOOKSTORE/src/main/java/com/capgemini/onlinebook/dao/BookDetailsDao.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.onlinebook.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capgemini.onlinebook.entity.BookDetails;
-
-public interface BookDetailsDao extends JpaRepository<BookDetails, Integer> {
-
-}
